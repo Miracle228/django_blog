33c0fab7769f0a3611230c788d71e13763c115c6
Your Name <you@example.com>
2020-03-01 20:22:12 +0600

    User Registration

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
