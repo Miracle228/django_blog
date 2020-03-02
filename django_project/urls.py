daed5cfc35134b680c5b21261df4310f9fb85be0
Your Name <you@example.com>
2020-03-02 20:25:51 +0600

    profile pictures

--- django_project/urls.py
+++ django_project/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
 ]
 
 #
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
