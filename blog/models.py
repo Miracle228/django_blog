6393b6fab4ae257ab7feeb375c153b9f6f151000
Your Name <you@example.com>
2020-03-03 19:02:43 +0600

    Create, Update, and Delete Posts

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -12,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
